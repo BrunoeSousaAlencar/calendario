1d9501705785c377779c5f0d8e3e9f3f459a08b2
Bruno e Sousa Alencar <brunoesousaalencar@gmail.com>
2022-06-24 17:45:22 -0300

    em andamento 1

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
+//topo do calendário
 now = new Date
 
-semana = new Array("Domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado")
+semana = new Array ("Domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado")
 
 mes = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro")
 
-document.getElementById("data").innerHTML = (now.getDate()+', ' + semana[now.getDay()]+'-feira , '+ mes [now.getDay()]+ " de " + now.getFullYear())
+document.getElementById("mes").innerHTML = (mes [now.getDay()])
+
+document.getElementById("data").innerHTML = (now.getDate()+" " + semana[now.getDay()] + '-feira' + " de " + now.getFullYear())
+//fim do calendário
+
+// dias do mês
+hoje = now.getDay()
+
+if( hoje == 0 )
+    document.getElementById("dom").innerHTML = ("#####")
+
+if( hoje == 1 )
+    document.getElementById('sex').style.backgroundColor = "#85C8F2";
+    document.getElementById('sex').style.borderRadius = '5px';
+
+if( hoje == 2 )
+    document.getElementById('sex').style.backgroundColor = "#85C8F2";
+    document.getElementById('sex').style.borderRadius = '5px';
+
+if( hoje == 3 )
+    document.getElementById('sex').style.backgroundColor = "#85C8F2";
+    document.getElementById('sex').style.borderRadius = '5px';
+
+if( hoje == 4 )
+    document.getElementById('sex').style.backgroundColor = "#85C8F2";
+    document.getElementById('sex').style.borderRadius = '5px';
+
+if( hoje == 5 )
+    document.getElementById('sex').style.backgroundColor = "#85C8F2";
+    document.getElementById('sex').style.borderRadius = '5px';
+    
+if( hoje == 6 )
+    document.getElementById('sex').style.backgroundColor = "#85C8F2";
+    document.getElementById('sex').style.borderRadius = '5px';
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//fim do dia do mês
